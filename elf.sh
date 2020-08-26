9cfde3eeed2e60550253ff8dc11a6dd21483b7bc
Bryan Dollery <bryan.dollery@knowledgehut.co>
2020-08-26 09:14:00 +0000

    refactored cluster creation into a different repo

--- elf.sh
+++ elf.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-k3d cluster create elf -v /etc/machine-id:/etc/machine-id:ro -v /var/log/journal:/var/log/journal:ro -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0]
+#
 kubectl apply -f elf.namespace.yaml
 helm repo add elastic https://helm.elastic.co
 helm repo add fluent https://fluent.github.io/helm-charts
